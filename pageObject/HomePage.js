223361a011e0f2291a23c65408c53c129c1bf30f
v-barladyn <37657435+v-barladyn@users.noreply.github.com>
2018-08-29 11:30:46 +0300

    Update HomePage.js

--- pageObject/HomePage.js
+++ pageObject/HomePage.js
@@ -3,8 +3,7 @@ let WebTextInput = require("../controls/web.input");
 let WebTextView = require("../controls/web.label");
 
 class HomePage {
-    constructor(){ 
-        
+    constructor(){         
 
         this.adminTab = new WebButton(element(by.xpath("//a[contains(text(),'ADMINISTRATION')]")), "Admin Tab");
         this.productTab = new WebTextView(element(by.xpath("//a[contains(text(),'PRODUCTS')]")), "Product Tab");
@@ -16,7 +15,6 @@ class HomePage {
         this.save = new WebButton(element(by.css('#saveProductAdd')), "Save button");
         this.serchForProduct = new WebTextInput(element(by.xpath("//input[@name='searchValue']")), "Search for product by name");
         this.findProduct = new WebTextView(element(by.xpath('//a[contains(text(),' + this.productName + ')]')), "Find product");
-
       
         this.productName = 'vasylbarladyn';
         this.familyName = 'AQA';
@@ -25,11 +23,8 @@ class HomePage {
         this.password = "NbqwN28z8A+";
     }
 
-    async switchToAdminTab(){
-        browser.waitForAngularEnabled(true);
-           
-        await this.adminTab.click();
-        await this.inputProdactName.sendKeys(this.productName);
+    async switchToAdminTab(){          
+        await this.adminTab.click();       
 
         let label = await this.productTab.getLabel();
         expect(label).toEqual(this.tabLabel);  
@@ -41,9 +36,9 @@ class HomePage {
         await this.productFamilyArea.click();
         await this.searchProducFamily.sendKeys(this.familyName);
         await this.setProductFamily.click();
-        await this.save.click();
-         
+        await this.save.click();         
         await this.serchForProduct.sendKeys(this.productName);
+        
         let productName = await findProduct.getLabel();
         expect(productName).toEqual(this.productName); 
         
