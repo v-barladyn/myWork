4895d42a8579737ec9924553fdd5da7fb5cf1d7f
Vasyl Barladyn <vasyl.barladyn@eleks.com>
2018-08-29 16:59:33 +0300

    protractor3

--- test_specs/login_spec.js
+++ test_specs/login_spec.js
@@ -4,6 +4,7 @@ import LoginPage from  "../pageObject/LoginPage";
 describe('Login Functionality', () => {
 
     it('User should be able to login into eds_university.eleks.com', async () => {
+
        await LoginPage.logIn();
        await HomePage.switchToAdminTab();
        await HomePage.addNewProduct();       
